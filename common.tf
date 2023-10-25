2fbdc4b0c895332e84c928cc83dfb07051761582
Manuel Hutter <manuel@hutter.io>
2023-10-25 09:31:02 +0200

    Move networking-related stuff to separate file
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- common.tf
+++ common.tf
@@ -16,19 +16,6 @@ data "hcloud_image" "arm" {
   with_status       = ["available"]
 }
 
-resource "hcloud_network" "k3s" {
-  name     = "k3s"
-  ip_range = local.internal_network
-  labels   = local.common_labels
-}
-
-resource "hcloud_network_subnet" "k3s" {
-  network_id   = hcloud_network.k3s.id
-  network_zone = "eu-central"
-  ip_range     = local.internal_network
-  type         = "cloud"
-}
-
 resource "random_password" "agent_token" {
   length  = 64
   lower   = true
