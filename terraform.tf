cf56bae1ecbc3445e9936b3c005f36d8a621b5e7
Manuel Hutter <manuel@hutter.io>
2024-06-26 09:58:37 +0200

    Update deprecated provider options
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ terraform {
     # AWS_S3_ENDPOINT
 
     # cloudscale.ch compatibility
-    force_path_style            = true
+    use_path_style              = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
